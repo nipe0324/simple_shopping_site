3d00059e6c67f7ec033525a00d8d01fbb87e0a50
nipe0324 <nipe0324@gmail.com>
2013-10-04 23:45:23 +0900

    Changed style of product index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(
+	name: 'オムライス',
+	description: 'とっても美味しいオムライス',
+	image_url: 'omurice.jpeg',
+	price: '800',
+	status: 'finished',
+  note: '舌が落ちるのに注意')
+
+Product.create(
+	name: 'アヒージョ',
+	description: 'オリーブオイル！',
+	image_url: 'ahijo.jpeg',
+	price: '600',
+	status: 'finished')
+
+Product.create(
+	name: 'ラザニア',
+	description: '重ねれば重ねるほど美味しい',
+	image_url: 'razania.jpeg',
+	price: '500',
+	status: 'finished')
