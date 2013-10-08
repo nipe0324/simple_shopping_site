3274bac68af04b63aee8eceef857bcac38fa2fc2
nipe0324 <nipe0324@gmail.com>
2013-10-08 23:17:58 +0900

    added view flow of devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable, :confirmable, :lockable
+
+  validates :name, :address, presence: true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,17 @@ CharityBazaar::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   # Email for Devise gem
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # SMTP Delivery by Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'devnul123qwe',
+    password:             'zxcvbnm,./_',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
